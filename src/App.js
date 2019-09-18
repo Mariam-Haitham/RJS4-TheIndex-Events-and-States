a3e920d82a755a2bde5d53fb44af05e0c77b1866
Mariam <Mariamhh98@hotmail.com>
2019-09-18 16:02:56 +0300

    Sara and Mariam

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Data
 import authors from "./data";
@@ -6,20 +6,60 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
+import AuthorDetail from "./AuthorDetail";
 
-function App() {
-  return (
-    <div id="app" className="container-fluid">
-      <div className="row">
-        <div className="col-2">
-          <Sidebar />
-        </div>
-        <div className="content col-10">
-          <AuthorsList authors={authors} />
+class App extends Component {
+  state = {
+    currentAuthor: null,
+    filteredAuthors: authors
+  };
+
+  selectAuthor = author => {
+    this.setState({ currentAuthor: author });
+  };
+
+  unselectAuthor = () => {
+    this.setState({ currentAuthor: null });
+  };
+
+  view = () => {
+    if (this.state.currentAuthor) {
+      return <AuthorDetail author={this.state.currentAuthor} />;
+    } else {
+      return (
+        <AuthorsList
+          authors={this.state.filteredAuthors}
+          selectAuthor={this.selectAuthor}
+          filterAuthors={this.filterAuthors}
+        />
+      );
+    }
+  };
+
+  filterAuthors = query => {
+    query = query.toLowerCase();
+
+    let filteredAuthors = authors.filter(author => {
+      return `${author.first_name} ${author.last_name}`
+        .toLowerCase()
+        .includes(query);
+    });
+
+    this.setState({ filteredAuthors: filteredAuthors });
+  };
+
+  render() {
+    return (
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar unselectAuthor={this.unselectAuthor} />
+          </div>
+          <div className="content col-10">{this.view()}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -1,16 +1,22 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 // Components
 import AuthorCard from "./AuthorCard";
 
 function AuthorsList(props) {
   const authorCards = props.authors.map(author => (
-    <AuthorCard key={author.first_name + author.last_name} author={author} />
+    <AuthorCard
+      key={author.first_name + author.last_name}
+      author={author}
+      selectAuthor={props.selectAuthor}
+    />
   ));
 
   return (
     <div className="authors">
       <h3>Authors</h3>
+      <SearchBar filterAuthors={props.filterAuthors} />
       <div className="row">{authorCards}</div>
     </div>
   );
